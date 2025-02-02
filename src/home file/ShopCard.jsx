8de108b017c613bf14a0f2dda6365f6c4c7dddf0
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-01 19:51:31 -0800

    added file

--- src/home file/ShopCard.jsx
+++ src/home file/ShopCard.jsx
@@ -1,19 +1,28 @@
-
+import { useEffect } from "react"
+import { useState } from "react"
+import ShopDetails from "./ShopDetails"
 
 function ShopCard() {
 
+    const [items, setItems] = useState([])
 
+    useEffect(() => {
+        fetch('products.json')
+            .then(res => res.json())
+            .then(data => setItems(data))
+    }, [])
 
     return (
-        <div className=" mt-20 mb-20 ml-12 mr-12 ">
+        <div className="  mt-20 mb-20 ml-12 mr-12 ">
+            <div className=" flex justify-center ">
+                <div className=" grid  md:grid-cols-3 gap-10 ">
+                    {
 
-            <div>
+                        items.map(items => <ShopDetails items={items}></ShopDetails>)
 
-
-
-
+                    }
+                </div>
             </div>
-
         </div>
     )
 }
--- src/home file/ShopDetails.jsx
+++ src/home file/ShopDetails.jsx
@@ -1,11 +1,33 @@
 
 
-function ShopDetails() {
+function ShopDetails({ items }) {
+
+    const { image, Title, Name, price } = items
+
     return (
         <div>
-
+            <div className="card bg-base-100 w-96 h-[600px] 
+            shadow-xl
+             hover:border-[1px] border-cyan-400
+            ">
+                <figure className="">
+                    <img
+                        src={image}
+                        alt="Shoes"
+                        className=" h-[400px] w-full" />
+                </figure>
+                <div className="card-body items-center text-center">
+                    <h2 className=" font-thin text-xs text-gray-400 ">{Title}</h2>
+                    <p className=" font-sans text-xl text-black "> {Name} </p>
+                    <p className=" text-xl font-sans "> {price} </p>
+                    <div className="card-actions">
+                        <button className="btn bg-cyan-800 text-white font-mono ">Add Cart</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
 
 export default ShopDetails
+
